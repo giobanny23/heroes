ecfa2957bc75fa8d8df01fabdd5db0949bae3062
giobanny23 <edgar_2_62@hotmail.com>
2021-01-13 20:59:17 -0600

    Linead separadora entre el listado

--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -5,6 +5,7 @@ export const MarvelScreen = () => {
     return (
         <div>
             <h1>Marvel Screen</h1>
+            <hr />
             <HeroList publisher ="Marvel Comics" />
         </div>
     )
