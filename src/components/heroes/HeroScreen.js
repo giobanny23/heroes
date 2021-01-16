87a3283612e39a6b5e271e6ea62cef4779efacf8
giobanny23 <edgar_2_62@hotmail.com>
2021-01-15 18:36:32 -0600

    Leer las url para que redirecciones a la pagina principal

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,6 +1,29 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router-dom'
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+
+    const { heroId } = useParams();
+
+
+    const hero = getHeroById(heroId);
+
+    if( !hero){
+        return <Redirect to="/" />
+    }
+
+    const{
+        superhero,
+        publihser,
+        alter_ego,
+        first_appearance,
+        characters,
+    } = hero;
+
+
+    
+
     return (
         <div>
             <h1>HeroScreen</h1>
--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -14,7 +14,7 @@ export const DashboardRoutes = () => {
                   
                 <Switch>
                     <Route exact path="/marvel" component={MarvelScreen} />
-                    <Route exact path="/heroe/:heroId" component={HeroScreen}/> 
+                    <Route exact path="/hero/:heroId" component={HeroScreen}/> 
                     <Route exact path="/dc" component={DcScreen} />
 
                     <Redirect to="/marvel" />  
