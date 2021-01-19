39314abc1037ed720e675c297e48e597c1ebc966
giobanny23 <edgar_2_62@hotmail.com>
2021-01-18 23:06:12 -0600

    agregando efectos con animate

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -34,7 +34,7 @@ export const HeroScreen = ({ history} ) => {
                 <img
                     src={`../assets/heroes/${heroId}.jpg`}
                     alt={superhero}
-                    className="img-thumbnail"
+                    className="img-thumbnail  animate__animated animate__fadeIn"
                     />
             </div>
             <div className="col-8">
